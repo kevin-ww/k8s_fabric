acafa825a4c0eb612a1e604370e7a92c1955958c
Kevin Wu <kevin.wu@magicwindow.cn>
2018-03-15 22:49:36 +0800

    fix mspPath issue

--- setupCluster/transform/generate.py
+++ setupCluster/transform/generate.py
@@ -8,48 +8,48 @@ BASEDIR = os.path.dirname(__file__)
 ORDERER = os.path.join(BASEDIR, "../crypto-config/ordererOrganizations")
 PEER = os.path.join(BASEDIR, "../crypto-config/peerOrganizations")
 
-#generateNamespacePod generate the yaml file to create the namespace for k8s, and return a set of paths which indicate the location of org files  
+# generateNamespacePod generate the yaml file to create the namespace for k8s, and return a set of paths which indicate the location of org files
+
 
 def generateNamespacePod(DIR):
-	orgs = []
-	for org in os.listdir(DIR):
-		orgDIR = os.path.join(DIR, org)
-		## generate namespace first.
-		tc.configORGS(org, orgDIR)
-		orgs.append(orgDIR)
-		#orgs.append(orgDIR + "/" + DIR.lower())
-	
-	#print(orgs)	
-	return orgs
+  orgs = []
+  for org in os.listdir(DIR):
+    orgDIR = os.path.join(DIR, org)
+    # generate namespace first.
+    tc.configORGS(org, orgDIR)
+    orgs.append(orgDIR)
+    #orgs.append(orgDIR + "/" + DIR.lower())
+
+  # print(orgs)
+  return orgs
 
 
 def generateDeploymentPod(orgs):
-	for org in orgs:
+  for org in orgs:
 
-		if org.find("peer") != -1: #whether it create orderer pod or peer pod 
-			suffix = "/peers"
-		else:
-			suffix = "/orderers"
+    if org.find("peer") != -1:  # whether it create orderer pod or peer pod
+      suffix = "/peers"
+    else:
+      suffix = "/orderers"
 
-		members = os.listdir(org + suffix)
-		for member in members:
-			#print(member)
-			memberDIR = os.path.join(org + suffix, member)
-			#print(memberDIR)
-			#print(os.listdir(memberDIR))
-			tc.generateYaml(member,memberDIR, suffix)
+    members = os.listdir(org + suffix)
+    for member in members:
+      # print(member)
+      memberDIR = os.path.join(org + suffix, member)
+      # print(memberDIR)
+      # print(os.listdir(memberDIR))
+      tc.generateYaml(member, memberDIR, suffix, ORDER, PEER)
 
 
-#TODO kafa nodes and zookeeper nodes don't have dir to store their certificate, must use anotherway to create pod yaml.
+# TODO kafa nodes and zookeeper nodes don't have dir to store their certificate, must use anotherway to create pod yaml.
 
 def allInOne():
-	peerOrgs = generateNamespacePod(PEER)
-	generateDeploymentPod(peerOrgs)
+  peerOrgs = generateNamespacePod(PEER)
+  generateDeploymentPod(peerOrgs)
 
-	ordererOrgs = generateNamespacePod(ORDERER)
-	generateDeploymentPod(ordererOrgs)
+  ordererOrgs = generateNamespacePod(ORDERER)
+  generateDeploymentPod(ordererOrgs)
 
 
-if __name__ == "__main__" :
-	allInOne()
-	
+if __name__ == "__main__":
+  allInOne()
